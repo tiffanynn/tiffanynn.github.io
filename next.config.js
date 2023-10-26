0f84ed4a7edd39bf1678053d3ba7e69d5abf6566
tiffanynn <tiffany.phuongn@gmail.com>
2023-10-26 00:28:45 -0700

    Edited next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+
+    // Add basePath
+    basePath: '/tiffanynn.github.io',
+}
 
 module.exports = nextConfig
